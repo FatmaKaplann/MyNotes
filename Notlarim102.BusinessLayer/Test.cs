eae537b76af0f5fafc30706a7f77ecb2105b05dd
FatmaKaplann <fatma.kaplan.9445@gmail.com>
2022-02-12 22:19:40 +0300

    Add project files.

---
+++ Notlarim102.BusinessLayer/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notlarim102.DataAccessLayer;
+
+namespace Notlarim102.BusinessLayer
+{
+    public class Test
+    {
+        public Test()
+        {
+            NotlarimContext db = new NotlarimContext();
+            db.Categories.ToList();
+            //db.Database.CreateIfNotExists();
+
+        }
+         
+        
+    }
+}
